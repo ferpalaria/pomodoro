e28f25b5f238c57fb1959e5d075a46a4859c6bc3
Fernanda Palaria <fernanda_palaria@terceiros.sicredi.com.br>
2023-03-05 02:42:20 -0300

    sounds

--- js/timer.js
+++ js/timer.js
@@ -1,50 +1,69 @@
+import Sounds from "./sounds"
+
 // Factory
-export function Timer({
+export default function Timer({
     // injeção de dependência
     minutesDisplay,
     secondsDisplay,
-    timerTimeOut,
-    resetControls // vem de outra lib, mas é uma dependência desse modulo
+    resetControls, // vem de outra lib, mas é uma dependência desse modulo
 }) {
 
-    function updateTimerDisplay(minutes, seconds) {
-        minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    let timerTimeOut
+    let minutes = Number(minutesDisplay.textContent)
+
+
+    function updateDisplay(newMinutes, seconds) {
+        newMinutes = newMinutes === undefined ? minutes : newMinutes
+        seconds = seconds === undefined ? 0 : seconds
+        minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
         secondsDisplay.textContent = String(seconds).padStart(2, "0")
     }
 
-    function resetTimer() {
-        updateTimerDisplay(minutes, 0)
-        clearTimeout(timerTimeOut)
+    function reset() {
+        updateDisplay(minutes, 0)
+        hold()
     }
 
     function countdown() {
         timerTimeOut = setTimeout(function () {
             let seconds = Number(secondsDisplay.textContent)
             let minutes = Number(minutesDisplay.textContent)
+            let isFinished = minutes <= 0 && seconds <= 0
 
-            updateTimerDisplay(minutes, 0)
+            updateDisplay(minutes, 0)
 
-            if (minutes <= 0) {
+            if (isFinished) {
                 resetControls()
+                updateDisplay()
+                Sounds().timeEnd()
                 return
             }
-
             if (seconds <= 0) {
-                seconds = 2
+                seconds = 60
                 --minutes
             }
 
-            updateTimerDisplay(minutes, String(seconds - 1))
+            updateDisplay(minutes, String(seconds - 1))
 
             countdown()
 
         }, 1000)
     }
 
+    function updateMinutes(newMinutes) {
+        minutes = newMinutes
+    }
+
+    function hold() {
+        clearTimeout(timerTimeOut)
+    }
+
     return {
-        updateTimerDisplay,
-        resetTimer,
-        countdown
+        updateDisplay,
+        reset,
+        countdown,
+        updateMinutes,
+        hold
     }
 }
 
